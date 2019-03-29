f67392d36be769c9cda63e7ac4e78634488d13ff
Roshoy <amaciej@gmail.com>
2019-03-29 03:48:38 +0100

    a

---
+++ cw04/zad1/main_sh.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int stopped = 0;
+
+void print_timestamp(){
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf("%d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1,
+        tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+}
+
+void au2(int signum){
+    exit(0);
+}
+
+void au(int signum){
+    
+    if(!stopped){
+        printf("Oczekuję na CTRL+Z - kontynuacja albo CTR+C - zakończenie programu\n");
+        stopped = 1;
+    }else{
+        printf("Kontynuacja\n");
+        stopped = 0;
+    }        
+}
+
+int main(int argc, char **argv){
+    signal(SIGTSTP, au);
+    signal(SIGINT, au2);
+        
+    //while(1) {
+        int child = fork();
+        if(child == 0){
+            execl("/bin/sh","while true; do date; done",NULL);
+        }else{
+            
+                wait(NULL);
+            
+        }
+
+   // }
+    return 0;
+}
